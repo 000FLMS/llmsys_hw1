1445949d25cec568ca12322b0c9a6f43e9880030
000FLMS <sihanhe@andrew.cmu.edu>
2026-01-19 17:25:38 -0500

    Zip kernel

--- src/combine.cu
+++ src/combine.cu
@@ -342,7 +342,16 @@ __global__ void zipKernel(
   // 7.Calculate the position of element in b_array according to b_index and b_strides
   // 8. Apply the binary function to the input elements in a_array & b_array and write the output to the out memory
 
-  assert(false && "Not Implemented");
+  int cur_idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (cur_idx < out_size) {
+    to_index(cur_idx, out_shape, out_index, out_shape_size);
+    int out_pos = index_to_position(out_index, out_strides, out_shape_size);
+    broadcast_index(out_index, out_shape, a_shape, a_index, out_shape_size, a_shape_size);
+    broadcast_index(out_index, out_shape, b_shape, b_index, out_shape_size, b_shape_size);
+    int a_pos = index_to_position(a_index, a_strides, a_shape_size);
+    int b_pos = index_to_position(b_index, b_strides, b_shape_size);
+    out[out_pos] = fn(fn_id, a_storage[a_pos], b_storage[b_pos]);
+  }
   /// END HW1_2
 }
 
